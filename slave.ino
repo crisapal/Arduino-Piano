5e1a80e559bac545ab1a95deeb51d08f2abc504a
Cristina P <32393477+crisapal@users.noreply.github.com>
2020-12-16 19:51:18 +0200

    Add files via upload

--- slave.ino
+++ slave.ino
@@ -5,9 +5,9 @@ byte x = 0;
 
 void setup()
 {
-  Serial.begin(9600);           // start serial for output
- Wire.begin(9);                // join i2c bus with address #4
- Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           
+ Wire.begin(9);                
+ Wire.onReceive(receiveEvent); 
  Serial.println("Saracia");
 }
 
